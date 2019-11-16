34e9e4f1c222d9566067fcd60cb0cdd5c1ddfb7b
a.binder <a.binder@biall-net.pl>
2019-11-16 12:54:01 +0100

    web

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,17 @@
+define(function() {var keywords=[{w:"Abstract",p:["p0"]},{w:"Nie",p:["p1"]},{w:"odosobione",p:["p1"]},{w:"po\u017C\u0105dania",p:["p1","p3"]},{w:"Jam",p:["p2"]},{w:"jest",p:["p2"]},{w:"ten",p:["p2"]},{w:"kt\u00F3ry",p:["p2"]},{w:"rozpu\u015Bci\u0142",p:["p2"]},{w:"si\u0119",p:["p2"]},{w:"w",p:["p2"]},{w:"eon",p:["p2"]},{w:"Pr\u00F3\u017Cne",p:["p3"]},{w:"drugiej",p:["p3"]},{w:"strony",p:["p3"]},{w:"Badanie",p:["p4"]},{w:"limit\u00F3w",p:["p4"]},{w:"Geneza",p:["p5"]},{w:"Nasycenie",p:["p6"]},{w:"systemu",p:["p6","p8"]},{w:"Przes\u0142anie",p:["p7"]},{w:"Pod\u0142\u0105czeni",p:["p8"]},{w:"do",p:["p8"]},{w:"Zawarto\u015B\u0107",p:["p9"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1, 2, 3];
+ph["p2"]=[4, 5, 6, 7, 8, 9, 10, 11];
+ph["p3"]=[12, 3, 13, 14];
+ph["p4"]=[15, 16];
+ph["p5"]=[17];
+ph["p6"]=[18, 19];
+ph["p7"]=[20];
+ph["p8"]=[21, 22, 19];
+ph["p9"]=[23];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
